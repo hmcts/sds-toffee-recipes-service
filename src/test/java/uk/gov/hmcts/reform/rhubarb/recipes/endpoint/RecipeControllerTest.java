2c785ce53e8edda1b9a2e66a665d4c0e7ab3e263
Tim Jacomb <21194782+timja@users.noreply.github.com>
2022-01-17 09:27:28 +0000

    DTSPO-6151 Add code coverage (#29)

---
+++ src/test/java/uk/gov/hmcts/reform/rhubarb/recipes/endpoint/RecipeControllerTest.java
@@ -0,0 +1,109 @@
+package uk.gov.hmcts.reform.rhubarb.recipes.endpoint;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.ext.ScriptUtils;
+import org.testcontainers.jdbc.JdbcDatabaseDelegate;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import uk.gov.hmcts.reform.rhubarb.recipes.domain.Recipe;
+import uk.gov.hmcts.reform.rhubarb.recipes.domain.RecipeList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Testcontainers
+@SpringBootTest(properties = { "spring.flyway.enabled=true" })
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(initializers = {RecipeControllerTest.Initializer.class})
+class RecipeControllerTest {
+
+    @Container
+    public static final PostgreSQLContainer POSTGRES_CONTAINER = new PostgreSQLContainer("postgres:11-bullseye")
+        .withDatabaseName("toffee")
+        .withUsername("toffee")
+        .withPassword("toffee");
+
+    @Autowired
+    @SuppressWarnings("PMD.BeanMembersShouldSerialize")
+    private MockMvc mvc;
+
+    @Autowired
+    @SuppressWarnings("PMD.BeanMembersShouldSerialize")
+    private ObjectMapper objectMapper;
+
+    private static boolean initComplete;
+
+    static class Initializer
+        implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                "spring.datasource.url=" + POSTGRES_CONTAINER.getJdbcUrl(),
+                "spring.datasource.username=" + POSTGRES_CONTAINER.getUsername(),
+                "spring.datasource.password=" + POSTGRES_CONTAINER.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+
+    @BeforeEach
+    void before() {
+        if (!initComplete) {
+            try (JdbcDatabaseDelegate jdbcDatabaseDelegate = new JdbcDatabaseDelegate(POSTGRES_CONTAINER, "")) {
+                ScriptUtils.runInitScript(jdbcDatabaseDelegate, "init.sql");
+                initComplete = true;
+            }
+        }
+    }
+
+    @Test
+    void retrieveAll() throws Exception {
+        String result = mvc.perform(
+                get("/recipes")
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content()
+                .contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
+            ).andReturn()
+            .getResponse().getContentAsString();
+
+        RecipeList recipeList = objectMapper.readValue(result, RecipeList.class);
+
+        List<Recipe> recipes = recipeList.getRecipes();
+        assertEquals(1, recipes.size(), "correct number of recipes");
+        assertEquals("Toffee", recipes.get(0).name(), "title is as expected");
+    }
+
+    @Test
+    void retrieveOne() throws Exception {
+        String result = mvc.perform(
+                get("/recipes/1")
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content()
+                .contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
+            ).andReturn()
+            .getResponse().getContentAsString();
+
+        Recipe recipe = objectMapper.readValue(result, Recipe.class);
+
+        assertEquals("Toffee", recipe.name(), "title is as expected");
+        assertEquals("200g sugar", recipe.ingredients(), "ingredients are as expected");
+    }
+}
