12ca50a462c7e220207a33c59c5f7e01a3793471
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2023-04-12 17:05:19 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,13 @@ provider "azurerm" {
   features {}
 }
 
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  alias                      = "postgres_network"
+  subscription_id            = var.aks_subscription_id
+}
+
 locals {
   app        = "recipe-backend"
   create_api = var.env != "preview" && var.env != "spreview"
@@ -62,6 +69,18 @@ resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 
+resource "azurerm_key_vault_secret" "POSTGRES-USER-V14" {
+  name         = "recipe-backend-POSTGRES-USER-v14"
+  value        = module.postgresql_flexible.username
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS-V14" {
+  name         = "recipe-backend-POSTGRES-PASS-v14"
+  value        = module.postgresql_flexible.password
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
 module "recipe-database" {
   source             = "git@github.com:hmcts/cnp-module-postgres?ref=postgresql_tf"
   product            = var.product
@@ -82,4 +101,27 @@ module "recipe-database" {
   business_area      = "SDS"
 }
 
+module "postgresql_flexible" {
+    providers = {
+    azurerm.postgres_network = azurerm.postgres_network
+  }
+
+  source        = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=postgres-vnet-provider"
+  env           = var.env
+  product       = var.product
+  name          = "${var.product}-v14-flexible"
+  component     = var.component
+  business_area = "SDS"
+  location      = var.location
+
+  common_tags = var.common_tags
+  admin_user_object_id = var.jenkins_AAD_objectId
+  pgsql_databases = [
+    {
+      name : "toffee"
+    }
+  ]
+
+  pgsql_version = "14"
+}
 
