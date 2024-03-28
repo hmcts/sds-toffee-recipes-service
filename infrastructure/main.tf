5c6eb5b9b87325189eba5c5ab73b433d074efc21
Hannah Smith <13995068+hannah38@users.noreply.github.com>
2024-03-28 16:45:10 +0000

    Postgresql v15 (#428)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -88,6 +88,7 @@ module "postgresql_flexible" {
   component     = var.component
   business_area = "sds"
   location      = var.location
+  create_mode   = "Update"
 
   common_tags = var.common_tags
   admin_user_object_id = var.jenkins_AAD_objectId
@@ -97,5 +98,5 @@ module "postgresql_flexible" {
     }
   ]
 
-  pgsql_version = "14"
+  pgsql_version = "15"
 }
