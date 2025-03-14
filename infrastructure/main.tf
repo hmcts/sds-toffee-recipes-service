0d644529cbef70d3ee8eab5bc4ee7684e3e6fb8d
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-03-14 11:26:06 +0000

    add restore time (#636)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -117,6 +117,7 @@ module "postgresql_flexible_restore" {
   location         = var.location
   create_mode      = "PointInTimeRestore"
   source_server_id = module.postgresql_flexible.instance_id
+  restore_time     = timeadd(timestamp(), "24h") 
 
   common_tags          = var.common_tags
   admin_user_object_id = var.jenkins_AAD_objectId
