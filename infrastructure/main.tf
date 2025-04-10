91d64bf401d21bff792dd61c2fc1d357b6c90da6
Ieuan Byers <ieuan.byers@justice.gov.uk>
2025-04-10 15:15:47 +0100

    Create temporary restored server in aat

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -101,3 +101,32 @@ module "postgresql_flexible" {
   pgsql_version = "15"
   pgsql_sku     = var.pgsql_sku
 }
+
+module "postgresql_flexible_temp_restore" {
+  count = var.env == "aat" ? 1 : 0
+  providers = {
+    azurerm.postgres_network = azurerm.postgres_network
+  }
+
+  source           = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  env              = var.env
+  product          = var.product
+  name             = "${var.product}-v14-flexible"
+  component        = var.component
+  business_area    = "sds"
+  location         = var.location
+  create_mode      = "PointInTimeRestore"
+  restore_time     = timestamp() # UTC time
+  source_server_id = module.postgresql_flexible.id
+
+  common_tags          = var.common_tags
+  admin_user_object_id = var.jenkins_AAD_objectId
+  pgsql_databases = [
+    {
+      name : "toffee"
+    }
+  ]
+
+  pgsql_version = "15"
+  pgsql_sku     = var.pgsql_sku
+}
