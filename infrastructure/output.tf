794ae471705cc372e60bf7270bbd329131434363
manohar.nunna <manohar.nunna@hmcts.net>
2026-03-09 14:41:18 +0000

    DTSPO-30615: remove identity output - no longer in ASP module

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -7,8 +7,3 @@ output "app_service_plan_id" {
   description = "Resource ID of the toffee App Service Plan."
   value       = module.app_service_plan.asp_id
 }
-
-output "app_service_plan_identity_principal_id" {
-  description = "Principal ID of the system-assigned managed identity on the toffee App Service Plan."
-  value       = module.app_service_plan.asp_identity_principal_id
-}
