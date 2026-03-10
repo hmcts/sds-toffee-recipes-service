ff7209b200193b8734f546ee2ed30dd8a2bc2a6f
manohar.nunna <manohar.nunna@hmcts.net>
2026-03-10 12:32:14 +0000

    Update app-service-plan module ref to DTSPO-30614 and replace linux with os_type

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -105,13 +105,12 @@ module "postgresql_flexible" {
 }
 
 module "app_service_plan" {
-  source = "git@github.com:hmcts/cnp-module-app-service-plan?ref=DTSPO-30615-modernise-app-service-plan-module"
+  source = "git@github.com:hmcts/cnp-module-app-service-plan?ref=DTSPO-30614-modernise-app-service-plan-module"
 
   asp_name            = var.product
   env                 = var.env
   location            = var.location
   resource_group_name = local.shared_infra_rg
-  linux               = true
   asp_sku_size        = var.asp_sku_size
   asp_capacity        = var.asp_capacity
   common_tags         = var.common_tags
